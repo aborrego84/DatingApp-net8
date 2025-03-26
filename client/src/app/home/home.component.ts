908c831bca7b1299b0c486a3b01ad9e74cced1ec
Angel Borrego <angel.borrego@gmail.com>
2025-03-26 09:51:35 -0400

    End of section 9

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -9,14 +9,9 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit {
-  http = inject(HttpClient); // inject(HttpClient);
+export class HomeComponent {
   registerMode = false;
-  users: any;
 
-  ngOnInit(): void {
-    this.getUsers();
-  }
   registerToggle(){
     this.registerMode = !this.registerMode;
   }
@@ -24,11 +19,4 @@ export class HomeComponent implements OnInit {
     this.registerMode = event;
   }
 
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error=> console.log(error),
-      complete: () => console.log('Request has completed')
-    })
-  }
 }
